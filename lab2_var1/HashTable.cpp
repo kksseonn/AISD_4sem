ad2c6e976a0660890d512b2e7ca6ec73979274a2
kksseonn <kote13241@gmail.com>
2024-05-05 18:42:07 +0400

    fixed

--- lab2_var1/HashTable.cpp
+++ lab2_var1/HashTable.cpp
@@ -2,72 +2,97 @@
 #include <stdexcept>
 #include "HashTable.h"
 
+
 template<typename Key, typename Value>
-HashTable<Key, Value>::HashTable(size_t size) {
-	if (size == 0) 
-		throw std::invalid_argument("");
-	_data.resize(size);
+size_t HashTable<Key, Value>::hash(const Key& key) const {
+	return std::hash<Key>{}(key) % capacity;
 }
 
 template<typename Key, typename Value>
-HashTable<Key, Value>::HashTable(size_t size, const std::vector<std::pair<Key, Value>>& values) : _data(size) {
+HashTable<Key, Value>::HashTable(size_t size) : capacity(size) {
+	if (size == 0)
+		throw std::invalid_argument("Size must be greater than 0");
+	_data.resize(size, nullptr);
+}
+
+template<typename Key, typename Value>
+HashTable<Key, Value>::HashTable(size_t size, const std::vector<std::pair<Key, Value>>& values) : HashTable(size) {
 	for (const auto& pair : values) {
 		insert(pair.first, pair.second);
 	}
 }
 
 template<typename Key, typename Value>
-HashTable<Key, Value>::HashTable(const HashTable& other) : _data(other._data) {}
+HashTable<Key, Value>::HashTable(const HashTable& other) : capacity(other.capacity), _data(other._data.size()) {
+	for (size_t i = 0; i < other._data.size(); ++i) {
+		Pair* current = other._data[i];
+		while (current) {
+			insert(current->key, current->value);
+			current = current->next;
+		}
+	}
+}
 
 template<typename Key, typename Value>
-HashTable<Key, Value>::~HashTable() {}
+HashTable<Key, Value>::~HashTable() {
+	for (size_t i = 0; i < _data.size(); ++i) {
+		Pair* current = _data[i];
+		while (current) {
+			Pair* temp = current;
+			current = current->next;
+			delete temp;
+		}
+	}
+}
 
 template<typename Key, typename Value>
 HashTable<Key, Value>& HashTable<Key, Value>::operator=(const HashTable& other) {
 	if (this != &other) {
-		_data = other._data;
+		capacity = other.capacity;
+		_data.clear();
+		_data.resize(other._data.size());
+		for (size_t i = 0; i < other._data.size(); ++i) {
+			Pair* current = other._data[i];
+			while (current) {
+				insert(current->key, current->value);
+				current = current->next;
+			}
+		}
 	}
 	return *this;
 }
 
 template<typename Key, typename Value>
 void HashTable<Key, Value>::print() {
-	for (const auto& pair : _data) {
-		if (pair.filled) {
-			std::cout << pair.key << ": " << pair.value << std::endl;
+	for (size_t i = 0; i < _data.size(); ++i) {
+		Pair* current = _data[i];
+		while (current) {
+			std::cout << current->key << ": " << current->value << std::endl;
+			current = current->next;
 		}
 	}
 }
 
 template<typename Key, typename Value>
 void HashTable<Key, Value>::insert(const Key& key, const Value& value) {
-	size_t index = hash(key) % _data.size();
-	if (_data[index].filled) {
-		throw std::runtime_error("Table is full");
-	}
-	_data[index].key = key;
-	_data[index].value = value;
-	_data[index].filled = true;
+	size_t index = hash(key);
+	Pair* newNode = new Pair(key, value);
+	newNode->next = _data[index];
+	_data[index] = newNode;
 }
 
 template<typename Key, typename Value>
 void HashTable<Key, Value>::insert_or_assign(const Key& key, const Value& value) {
-	Pair* pair = search(key);
-	if (pair != nullptr) {
-		pair->value = value;
-		pair->filled = true;
+	size_t index = hash(key);
+	Pair* current = _data[index];
+	while (current) {
+		if (current->key == key) {
+			current->value = value;
+			return;
+		}
+		current = current->next;
 	}
-	else {
-		size_t i = 0;
-		size_t index;
-		do {
-			index = (hash(key) + i) % _data.size();
-			++i;
-		} while (_data[index].filled);
-		_data[index].key = key;
-		_data[index].value = value;
-		_data[index].filled = true;
-	}
+	insert(key, value);
 }
 
 template<typename Key, typename Value>
@@ -77,25 +102,38 @@ bool HashTable<Key, Value>::contains(const Key& key) {
 
 template<typename Key, typename Value>
 bool HashTable<Key, Value>::erase(const Key& key) {
-	Pair* pair = search(key);
-	if (!pair) return false;
-	pair->filled = false;
-	return true;
+	size_t index = hash(key);
+	Pair* current = _data[index];
+	Pair* prev = nullptr;
+	while (current) {
+		if (current->key == key) {
+			if (prev)
+				prev->next = current->next;
+			else
+				_data[index] = current->next;
+			delete current;
+			return true;
+		}
+		prev = current;
+		current = current->next;
+	}
+	return false;
 }
 
 template<typename Key, typename Value>
 int HashTable<Key, Value>::count(const Key& key) {
-	Pair* pair = search(key);
-	return (pair != nullptr && pair->filled) ? 1 : 0;
+	Pair* current = search(key);
+	return current ? 1 : 0;
 }
 
 template<typename Key, typename Value>
 typename HashTable<Key, Value>::Pair* HashTable<Key, Value>::search(const Key& key) {
-	for (size_t i = 0; i < _data.size(); ++i) {
-		size_t index = (hash(key) + i) % _data.size();
-		if (_data[index].filled && _data[index].key == key) {
-			return &_data[index];
-		}
+	size_t index = hash(key);
+	Pair* current = _data[index];
+	while (current) {
+		if (current->key == key)
+			return current;
+		current = current->next;
 	}
 	return nullptr;
 }
--- lab2_var1/HashTable.h
+++ lab2_var1/HashTable.h
@@ -4,25 +4,27 @@
 
 template<typename Key, typename Value>
 class HashTable {
-	struct Pair {
-		Key key;
-		Value value;
-		bool filled;
-		Pair() : filled(false) {}
-	};
-	std::vector<Pair> _data;
+    struct Pair {
+        Key key;
+        Value value;
+        Pair* next;
+        Pair() : next(nullptr) {}
+        Pair(const Key& k, const Value& v) : key(k), value(v), next(nullptr) {}
+    };
+    std::vector<Pair*> _data;
+    size_t capacity;
+    size_t hash(const Key& key) const;
+    Pair* search(const Key& key);
 public:
-	HashTable(size_t size);
-	HashTable(size_t size, const std::vector<std::pair<Key, Value>>& values);
-	HashTable(const HashTable& other);
-	~HashTable();
-	HashTable& operator=(const HashTable& other);
-	void print();
-	void insert(const Key& key, const Value& value);
-	void insert_or_assign(const Key& key, const Value& value);
-	bool contains(const Key& key);
-	bool erase(const Key& key);
-	int count(const Key& key);
-private:
-	Pair* search(const Key& key);
+    HashTable(size_t size);
+    HashTable(size_t size, const std::vector<std::pair<Key, Value>>& values);
+    HashTable(const HashTable& other);
+    ~HashTable();
+    HashTable& operator=(const HashTable& other);
+    void print();
+    void insert(const Key& key, const Value& value);
+    void insert_or_assign(const Key& key, const Value& value);
+    bool contains(const Key& key);
+    bool erase(const Key& key);
+    int count(const Key& key);
 };
